ebf5dce9e03eda7997994bc80303720286d22615
Asadbek <a.nosirjonov@uic.group>
2022-11-15 17:48:03 +0500

    ref: link added for generic cards

--- components/card/index.jsx
+++ components/card/index.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React, { Fragment } from "react";
 import photo from "../../public/photos/cardBg.webp";
 import {
@@ -13,14 +14,31 @@ import {
   Wrapper,
 } from "./style";
 
-const Card = ({ value, type, comingSoonImg }) => {
+const Card = ({ value, type, comingSoonImg, comingSoonLink, link }) => {
   return (
     <Fragment>
       <Container type={type} key={value?.id}>
         <Wrapper>
           <Status type={type}>{value?.status || "New"}</Status>
-          {type !== "comingSoon" ? (
-            <>
+          {type === "comingSoon" ? (
+            <Link href={comingSoonLink || "#"}>
+              <ComingSoonBlur />
+              <Image
+                src={
+                  comingSoonImg ||
+                  "https://www.masterclass.com/course-images/attachments/sjiytn0p38hi9w4gcah272emt6k4?width=1920&quality=75&format=webp"
+                }
+                width={516}
+                height={290}
+                alt="Coming soon photo"
+              />
+              <ContentForComingSoon>
+                <ComingSoonTitle>Mindy Weiss</ComingSoonTitle>
+                <p>Plan Your Dream Wedding</p>
+              </ContentForComingSoon>
+            </Link>
+          ) : (
+            <Link href={link || "#"}>
               <Image
                 className="img"
                 width={283}
@@ -33,24 +51,7 @@ const Card = ({ value, type, comingSoonImg }) => {
                 <div className="w-[40px] mb-9 h-[4px] bg-white"></div>
                 <Subtitle>{value?.subtitle || "Draw and Paint"}</Subtitle>
               </Content>
-            </>
-          ) : (
-            <div>
-              <ComingSoonBlur />
-              <Image
-                src={
-                  comingSoonImg ||
-                  "https://www.masterclass.com/course-images/attachments/sjiytn0p38hi9w4gcah272emt6k4?width=1920&quality=75&format=webp"
-                }
-                width={516}
-                height={290}
-                alt="coming soon"
-              />
-              <ContentForComingSoon>
-                <ComingSoonTitle>Mindy Weiss</ComingSoonTitle>
-                <p>Plan Your Dream Wedding</p>
-              </ContentForComingSoon>
-            </div>
+            </Link>
           )}
         </Wrapper>
       </Container>
